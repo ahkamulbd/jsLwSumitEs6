2123de567e178302989788383cf5fc9f96846c21
ahkamulbd <ahkamulbd@gmail.com>
2023-08-01 11:43:12 +0600

    Ternary operator addressed

---
+++ js/ternary.js
@@ -0,0 +1,22 @@
+let age = 11;
+// let type;
+
+// if (age >= 18) {
+//     type = 'adult';
+// }
+// else {
+//     type = 'child';
+// }
+
+//let type = (age >= 18) ? 'adult' : 'child';
+
+let type = (age >= 18) ? 'adult' : (age < 10) ? 'child' : 'teen';
+
+//console.log(type);
+
+let a = 3;
+
+//let access = a > 5 ? true : false;
+let access = a > 5;
+
+console.log(access);
