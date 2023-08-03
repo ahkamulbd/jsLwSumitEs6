059c01111a76884905ad22372a8604ae4190e3d3
ahkamulbd <ahkamulbd@gmail.com>
2023-08-03 09:23:12 +0600

    import & export addressed

---
+++ external.js
@@ -0,0 +1,21 @@
+// export const pi = 3.14;
+// export const a = 2.4;
+
+// /* ====== Exporting as default ======= */
+
+// export const pi = 3.14;
+// const a = 2.4;
+// export default a;
+
+
+// /* ====== Exporting function ======= */
+
+// export function myFunction() {
+//     console.log('Getting Function using import-export');
+// }
+
+/* ====== Exporting function as default ======= */
+
+export default function myFunction() {
+    console.log('Getting Function using import-export');
+}
---
+++ playground.js
@@ -0,0 +1,43 @@
+// import { pi, a } from './external.js'
+
+// console.log(pi, a);
+
+
+// /* ====== Importing as test ======== */
+
+// import * as test from './external.js'
+
+// console.log(test);
+// console.log(test.pi);
+
+
+// /* ====== Importing as varPI & varA ======== */
+
+// import { a as varA, pi as varPi } from './external.js'
+
+// console.log(varA, varPi);
+
+
+// /* ====== Importing as default ======== */
+
+// // import external from './external.js'
+// // console.log(external);
+
+// import external, { pi } from './external.js'
+// console.log(external, pi);
+
+
+// /* ====== Importing function ======== */
+
+// import { myFunction } from './external.js'
+
+
+// myFunction();
+
+
+/* ====== Importing function as default ======== */
+
+import external from './external.js'
+
+
+external();
